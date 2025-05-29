4a6d80652206984f64642e97ffb2a695e5e96a00
WuJingweiPayton <wu.jingwei.hkg@outlook.com>
2025-05-29 09:18:23 +0800

    提交描述（例如：修复字体路径问题）

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Geist, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const geist = Geist({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
